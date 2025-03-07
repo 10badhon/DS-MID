27feb0834f9e6b0d64cdd7840267986411a174d2
Tanvir Ahmed Badhon <151008110+10badhon@users.noreply.github.com>
2025-03-07 17:05:50 +0600

    lab8_2.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -12,15 +12,19 @@ int main(){
         cin>>arr[i];
     }
 
-    for(int i=0; i<n; i++){
-        int key=arr[i];
-        for(int j=i; j<n; j++){
-            if(arr[j]<key){
-                arr[i]=arr[j];
-                arr[j]=key;
-            }
+    for(int i=0; i<n-1; i++){
+        int min = i;
+        for(int j=i+1; j<n; j++){
+           if(arr[min]>arr[j]){
+              min=j;
+           }
         }
-    }
+        if(min != i){
+           int temp = arr[i];
+           arr[i] = arr[min];
+           arr[min] = temp; 
+        }
+     }
 
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
